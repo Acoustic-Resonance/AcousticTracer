87de00cc61acd557a1493b2bae59388f74c94314
EonMurph <stiger.home@gmail.com>
2026-02-02 15:34:43 +0000

    Add error handling...

--- core/src/at_bvh.c
+++ core/src/at_bvh.c
@@ -6,7 +6,16 @@
 
 AT_Result AT_minitree_create(AT_MiniTree **out_tree, AT_Triangle *triangles, uint32_t n)
 {
+    if (!out_tree || *out_tree || !triangles) return AT_ERR_INVALID_ARGUMENT;
+
     AT_MiniTree *mini_tree = malloc(sizeof(*mini_tree));
+    if (!mini_tree) {
+        // TODO: Deal with Allocation problems
+        // for now will just free triangles but later should try allocate again
+        free(triangles);
+        return AT_ERR_ALLOC_ERROR;
+    }
+
     mini_tree->triangles = triangles;
     mini_tree->n = n;
     mini_tree->aabb = AT_AABB_init();
