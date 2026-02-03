55120865cc0a446251f21705d114e79446a374b8
EonMurph <stiger.home@gmail.com>
2026-02-03 13:52:57 +0000

    Refactor to remove additional triangle arrays

--- core/src/at_trigroup.c
+++ core/src/at_trigroup.c
@@ -8,7 +8,7 @@ AT_Result AT_trigroup_create(AT_TriGroup **out_group, AT_Triangle *triangles, ui
     AT_TriGroup *tri_group = malloc(sizeof(*tri_group));
     if (!tri_group) {
         // TODO: Deal with Allocation problems
-        // for now will just free triangles but later should try allocate again
+        // for now will just return err but later should try allocate again
         return AT_ERR_ALLOC_ERROR;
     }
 
@@ -54,19 +54,14 @@ char get_longest_axis(const AT_TriGroup *group)
 
 AT_Result AT_trigroup_split(AT_TriGroup *org_group, AT_TriGroup **left_group, AT_TriGroup **right_group)
 {
+    // TODO: See about optimising so you don't need all the ifs
+
     if (!org_group || !left_group || *left_group || !right_group || *right_group) {
         return AT_ERR_INVALID_ARGUMENT;
     }
-    // TODO: Use org_group->triangles and just shift them around to reduce space and IO operations
-    // use https://youtu.be/C1H4zIiCOaI?t=692
-    AT_Triangle *triangles = malloc(sizeof(*triangles) * org_group->n);
-    if (!triangles) {
-        // TODO: Deal with allocation problems
-        return AT_ERR_ALLOC_ERROR;
-    }
-    // TODO: See about optimising so you don't need all the ifs
     // 1. Get longest axis
     char longest_axis = get_longest_axis(org_group);
+
     // 2. Get centre of longest axis
     float centre;
     if (longest_axis == 'x') {
@@ -76,68 +71,52 @@ AT_Result AT_trigroup_split(AT_TriGroup *org_group, AT_TriGroup **left_group, AT
     } else {
         centre = org_group->aabb.midpoint.z;
     }
+
     // 3. Get triangles to left of axis
     // 4. Get triangles to right of axis
+    AT_Triangle *triangles = org_group->triangles;
     uint32_t left = 0;
-    uint32_t right = org_group->n - 1;
+    AT_Triangle temp;
     for (uint32_t i = 0; i < org_group->n; i++) {
-        AT_Triangle triangle = org_group->triangles[i];
-        AT_Vec3 triangle_mid = triangle.aabb.midpoint;
+        AT_Vec3 triangle_mid = org_group->triangles[i].aabb.midpoint;
         if (longest_axis == 'x') {
             if (triangle_mid.x <= centre) {
-                triangles[left] = triangle;
+                if (left < i) {
+                    temp = triangles[left];
+                    triangles[left] = triangles[i];
+                    triangles[i] = temp;
+                }
                 left++;
-            } else {
-                triangles[right] = triangle;
-                right--;
             }
         } else if (longest_axis == 'y') {
             if (triangle_mid.y <= centre) {
-                triangles[left] = triangle;
+                if (left < i) {
+                    temp = triangles[left];
+                    triangles[left] = triangles[i];
+                    triangles[i] = temp;
+                }
                 left++;
-            } else {
-                triangles[right] = triangle;
-                right--;
             }
         } else {
             if (triangle_mid.z <= centre) {
-                triangles[left] = triangle;
+                if (left < i) {
+                    temp = triangles[left];
+                    triangles[left] = triangles[i];
+                    triangles[i] = temp;
+                }
                 left++;
-            } else {
-                triangles[right] = triangle;
-                right--;
             }
         }
     }
     int left_n = left;
-    int right_n = (org_group->n - 1) - right;
-    AT_Triangle *left_triangles = malloc(sizeof(*left_triangles) * left_n);
-    if (!left_triangles) {
-        // TODO: Deal with allocation problems
-        free(triangles);
-        return AT_ERR_ALLOC_ERROR;
-    }
-    AT_Triangle *right_triangles = malloc(sizeof(*right_triangles) * right_n);
-    if (!right_triangles) {
-        // TODO: Deal with allocation problems
-        free(triangles);
-        free(right_triangles);
-        return AT_ERR_ALLOC_ERROR;
-    }
-    for (int i = 0; i < left_n; i++) {
-        left_triangles[i] = triangles[i];
-    }
-    int j = 0;
-    for (int i = org_group->n - 1; i > right_n; i--) {
-        right_triangles[j] = triangles[i];
-        j++;
-    }
-    if (AT_trigroup_create(left_group, left_triangles, left_n) != AT_OK) {
+    int right_n = (org_group->n - 1) - left;
+    if (AT_trigroup_create(left_group, &triangles[0], left_n) != AT_OK) {
         perror("Failed to create left sub tree");
     }
-    if (AT_trigroup_create(right_group, right_triangles, right_n != AT_OK)) {
+    if (AT_trigroup_create(right_group, &triangles[left_n], right_n != AT_OK)) {
         perror("Failed to create right sub tree");
     }
+
     // 5. Repeat for sub trees
 
     AT_trigroup_destroy(org_group);
